a36ce9e2e216fad870d1b94879644487b7e7f508
Aryanjast13 <aryanjastaj05@gmail.com>
2025-08-07 19:07:35 +0530

    feat: contact  form ui  update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,15 @@
  
 const Contact = ({ scrollRef }) => {
   return (
-    <div ref={scrollRef} className="contact w-full flex flex-col md:flex-row px-4 pb-6">
+    <div
+      ref={scrollRef}
+      className="contact w-full flex flex-col md:flex-row px-4 pb-6"
+    >
       <div className="w-full md:w-[45%] mt-4  md:pb-30">
         <h3 className="text-7xl md:text-[10rem] ">Let's</h3>
-        <h3 className="text-7xl  md:text-[10rem] text-right mr-10 md:mr-0 leading-none ">Connect</h3>
+        <h3 className="text-7xl  md:text-[10rem] text-right mr-10 md:mr-0 leading-none ">
+          Connect
+        </h3>
       </div>
       <div className="w-full  md:w-[55%] ">
         <form className="flex flex-col mt-16 md:mt-32  md:ml-32 w-full md:w-2xl  space-y-8">
@@ -19,16 +24,25 @@ const Contact = ({ scrollRef }) => {
             placeholder="Email"
             className="border-b focus:outline-none"
           />
+          <input
+            type="text"
+            placeholder="Subject"
+            className="border-b focus:outline-none"
+          />
 
           <textarea
             type="text"
-            placeholder="description"
+            placeholder="message"
             className="border-b focus:outline-none"
           />
           <div className="flex justify-end pr-2 w-full">
-          <button type="submit" className="px-6 py-2 bg-[#777] rounded-full text-lg text-white w-fit">Submit</button>
-            
-           </div>
+            <button
+              type="submit"
+              className="px-6 py-2 bg-[#777] rounded-full text-lg text-white w-fit"
+            >
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </div>
